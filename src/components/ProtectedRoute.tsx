df25f8c6375c7859e24849ced578c5b32c5a22a7
Hiroshi Ito <itohiro73@gmail.com>
2020-08-31 21:27:17 +0900

    Use Omit to simplify route props definition.

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import * as H from "history";
-import { RouteChildrenProps, RouteComponentProps } from "react-router";
+import { RouteProps } from "react-router";
 import { WithAuthenticationRequiredOptions } from "@auth0/auth0-react/dist/with-authentication-required";
 import { Route } from "react-router-dom";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
@@ -10,15 +9,7 @@ interface ThisRouteComponentProps {
   component: React.ComponentType
 }
 
-interface ThisRouteProps {
-  location?: H.Location;
-  render?: (props: RouteComponentProps<any>) => React.ReactNode;
-  children?: ((props: RouteChildrenProps<any>) => React.ReactNode) | React.ReactNode;
-  path?: string | string[];
-  exact?: boolean;
-  sensitive?: boolean;
-  strict?: boolean;
-}
+type ThisRouteProps = Omit<RouteProps, 'component'>
 
 type ProtectedRouteProps = ThisRouteComponentProps & ThisRouteProps & WithAuthenticationRequiredOptions
 
@@ -27,7 +18,7 @@ const ProtectedRoute = <P extends object>(props: ProtectedRouteProps) => {
   const {returnTo, onRedirecting = () => <Loading />, loginOptions} = props
   const options = {returnTo, onRedirecting, loginOptions}
   const {location, render, children, path, exact, sensitive, strict} = props
-  const args = {location, render, children, path, exact, sensitive, strict}
+  const args: ThisRouteProps = {location, render, children, path, exact, sensitive, strict}
 
   return <Route component={withAuthenticationRequired(component, options)} {...args}/>
 }
