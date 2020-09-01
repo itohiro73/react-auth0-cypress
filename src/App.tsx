639a6d7f20c9d6e8b25b27a499be37fda99a448d
Hiroshi Ito <itohiro73@gmail.com>
2020-09-01 13:49:38 +0900

    Add tailwind and styled component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+import "./index.css";
+import AppStyles from "./AppStyles.styles.tw";
 import { Router, Switch } from 'react-router-dom';
 import { Auth0Provider } from "@auth0/auth0-react";
 import { createBrowserHistory } from 'history';
@@ -21,26 +22,29 @@ const onRedirectCallback = (appState: AppState) => {
 
 function App() {
   return (
-    <div className="App">
-      <Auth0Provider
-        domain={process.env.REACT_APP_AUTH0_DOMAIN ?? ''}
-        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID ?? ''}
-        redirectUri={window.location.origin}
-        onRedirectCallback={onRedirectCallback}
-      >
-        <Router history={history}>
-          <AuthWrapper>
-            <Switch>
-              <ProtectedRoute exact path="/" component={Home}/>
-              <ProtectedRoute path="/menu1" component={Menu1}/>
-              <ProtectedRoute path="/menu2" component={Menu2}/>
-              <ProtectedRoute path="/menu3" component={Menu3}/>
-              <ProtectedRoute path="/profile" component={Profile}/>
-            </Switch>
-          </AuthWrapper>
-        </Router>
-      </Auth0Provider>
-    </div>
+    <AppStyles>
+      <div className="App">
+        <Auth0Provider
+            domain={process.env.REACT_APP_AUTH0_DOMAIN ?? ''}
+            clientId={process.env.REACT_APP_AUTH0_CLIENT_ID ?? ''}
+            redirectUri={window.location.origin}
+            onRedirectCallback={onRedirectCallback}
+        >
+          <Router history={history}>
+            <AuthWrapper>
+              <Switch>
+                <ProtectedRoute exact path="/" component={Home}/>
+                <ProtectedRoute path="/menu1" component={Menu1}/>
+                <ProtectedRoute path="/menu2" component={Menu2}/>
+                <ProtectedRoute path="/menu3" component={Menu3}/>
+                <ProtectedRoute path="/profile" component={Profile}/>
+              </Switch>
+            </AuthWrapper>
+          </Router>
+        </Auth0Provider>
+      </div>
+    </AppStyles>
+
   );
 }
 
